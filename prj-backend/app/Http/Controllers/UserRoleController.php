b487d49f803d3a1ead4d0defa095dad4e279cb13
Denis18UKS <honorxpremium75@gmail.com>
2024-10-29 16:18:29 +0500

    фикс редактирования ролей пользователей

--- prj-backend/app/Http/Controllers/UserRoleController.php
+++ prj-backend/app/Http/Controllers/UserRoleController.php
@@ -9,17 +9,8 @@ use Illuminate\Support\Facades\Log;
 
 class UserRoleController extends Controller
 {
-    // Метод для назначения роли пользователю
     public function assignRole(Request $request, $userId)
     {
-        // Получаем роль из запроса или используем 'user' по умолчанию
-        $roleName = $request->input('role', 'user');
-        Log::info("Назначение роли: получено имя роли '{$roleName}' для пользователя с ID {$userId}");
-
-        // Проверяем существование роли
-        $role = Role::firstOrCreate(['name' => $roleName, 'guard_name' => 'web']);
-        Log::info("Роль найдена или создана: {$role->name}");
-
         // Находим пользователя по ID
         $user = User::find($userId);
         if (!$user) {
@@ -31,13 +22,26 @@ class UserRoleController extends Controller
         $user->syncRoles([]);
         Log::info("Роли пользователя с ID {$userId} успешно очищены.");
 
-        // Назначаем пользователю новую роль
-        $user->assignRole($role);
-        if ($user->hasRole($roleName)) {
+        // Получаем роль из запроса или используем 'user' по умолчанию
+        $roleName = $request->input('role', 'user');
+        Log::info("Назначение роли: получено имя роли '{$roleName}' для пользователя с ID {$userId}");
+
+        // Проверяем существование роли
+        $role = Role::firstOrCreate(['name' => $roleName, 'guard_name' => 'web']);
+        Log::info("Роль найдена или создана: {$role->name}");
+
+        try {
+            // Назначаем пользователю новую роль
+            $user->assignRole($role);
             Log::info("Роль '{$roleName}' успешно назначена пользователю с ID {$userId}.");
-            return response()->json(['message' => "Роль $roleName успешно назначена пользователю.", 'user' => $user, 'role' => $role]);
-        } else {
-            Log::error("Не удалось назначить роль '{$roleName}' пользователю с ID {$userId}.");
+
+            return response()->json([
+                'message' => "Роль $roleName успешно назначена пользователю.",
+                'user' => $user,
+                'role' => $role
+            ]);
+        } catch (\Exception $e) {
+            Log::error("Ошибка при назначении роли '{$roleName}' пользователю с ID {$userId}: " . $e->getMessage());
             return response()->json(['message' => "Не удалось назначить роль $roleName."], 500);
         }
     }
